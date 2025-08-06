ff3eae1f416becec345380e5da9bbba6f42852b9
venkatsandeepY <64055687+venkatsandeepY@users.noreply.github.com>
2025-08-06 19:46:21 +0530

    Remove redundant JSX fragment tags

--- src/pages/Status.jsx
+++ src/pages/Status.jsx
@@ -398,8 +398,6 @@ const Status = () => {
       </div>
 
       <div className="p-6">
-        <>
-        <>
         {/* Main Batch Status Table */}
         <div className="bg-white rounded-xl shadow-lg border border-gray-200 overflow-hidden">
           <div className="px-6 py-4 border-b border-gray-200 bg-gray-50">
@@ -587,8 +585,6 @@ const Status = () => {
           </div>
           )}
         </div>
-        </>
-        </>
       </div>
     </div>
   );
